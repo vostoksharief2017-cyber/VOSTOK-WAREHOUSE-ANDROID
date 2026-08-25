7bcceb4b4b18fe9484dedb651549f4b2460ed514
vostoksharief2017-cyber <vostoksharief2017@gmail.com>
2026-08-25 17:45:50 +0400

    Add files via upload

---
+++ VostokWarehouseMobile/App.xaml.cs
@@ -0,0 +1,9 @@
+namespace VostokWarehouseMobile;
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new NavigationPage(new MainPage());
+    }
+}
