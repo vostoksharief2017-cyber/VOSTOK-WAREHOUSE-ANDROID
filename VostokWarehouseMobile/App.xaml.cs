b863cc0680876732eddc077b13b9fe0038b11e6c
vostoksharief2017-cyber <vostoksharief2017@gmail.com>
2026-08-25 17:50:19 +0400

    Delete VostokWarehouseMobile directory

--- VostokWarehouseMobile/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace VostokWarehouseMobile;
-public partial class App : Application
-{
-    public App()
-    {
-        InitializeComponent();
-        MainPage = new NavigationPage(new MainPage());
-    }
-}
