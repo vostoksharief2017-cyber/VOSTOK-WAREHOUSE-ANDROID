c1490a5acdfa87f44d259ba25e12082e72d71d5f
vostoksharief2017-cyber <vostoksharief2017@gmail.com>
2026-08-26 10:12:05 +0400

    Add MainApplication class for Android platform

--- VostokWarehouseMobile/Platforms/Android/MainApplication.cs
+++ VostokWarehouseMobile/Platforms/Android/MainApplication.cs
@@ -1,1 +1,18 @@
+using Android.App;
+using Android.Runtime;
 
+namespace VostokWarehouseMobile;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
+
+    protected override MauiApp CreateMauiApp()
+    {
+        return MauiProgram.CreateMauiApp();
+    }
+}
