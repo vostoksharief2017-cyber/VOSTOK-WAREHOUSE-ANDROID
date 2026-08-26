db233669ea89a30730c0cdf3929237920a6285ba
vostoksharief2017-cyber <vostoksharief2017@gmail.com>
2026-08-26 15:02:54 +0400

    Update App.xaml.cs

--- VostokWarehouseMobile/App.xaml.cs
+++ VostokWarehouseMobile/App.xaml.cs
@@ -1,9 +1,15 @@
 namespace VostokWarehouseMobile;
+
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
-        MainPage = new NavigationPage(new MainPage());
+    }
+
+    protected override Window CreateWindow(
+        IActivationState? activationState)
+    {
+        return new Window(new MainPage());
     }
 }
