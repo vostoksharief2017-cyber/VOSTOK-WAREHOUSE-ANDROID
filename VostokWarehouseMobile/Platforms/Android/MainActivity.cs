346236d855a383bd0f435ed4643b44e4e90f523f
vostoksharief2017-cyber <vostoksharief2017@gmail.com>
2026-08-26 10:11:19 +0400

    Add MainActivity for Android platform

--- VostokWarehouseMobile/Platforms/Android/MainActivity.cs
+++ VostokWarehouseMobile/Platforms/Android/MainActivity.cs
@@ -1,1 +1,18 @@
+using Android.App;
+using Android.Content.PM;
 
+namespace VostokWarehouseMobile;
+
+[Activity(
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges =
+        ConfigChanges.ScreenSize |
+        ConfigChanges.Orientation |
+        ConfigChanges.UiMode |
+        ConfigChanges.ScreenLayout |
+        ConfigChanges.SmallestScreenSize |
+        ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
