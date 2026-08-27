2e7ed5a7b58f9a8497d2d555bf5203b91e93df6c
vostoksharief2017-cyber <vostoksharief2017@gmail.com>
2026-08-27 18:04:28 +0400

    Configure OpenSans fonts in MauiProgram
    Added font configuration for OpenSans fonts.

--- VostokWarehouseMobile/MauiProgram.cs
+++ VostokWarehouseMobile/MauiProgram.cs
@@ -7,7 +7,17 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
 
         builder
-            .UseMauiApp<App>();
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont(
+                    "OpenSans-Regular.ttf",
+                    "OpenSansRegular");
+
+                fonts.AddFont(
+                    "OpenSans-Semibold.ttf",
+                    "OpenSansSemibold");
+            });
 
         return builder.Build();
     }
